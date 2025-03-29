0cfe9cfb341e7ff953cfcef2d6d0433cd6df24e4
Silvia Cruz Blanco <silviacruz1801@gmail.com>
2025-03-29 11:58:31 +0100

    concurrency added

--- modules/DataLoader.py
+++ modules/DataLoader.py
@@ -2,6 +2,7 @@ from unstructured.partition.pdf import partition_pdf
 import os
 from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
 from PIL import ImageFile
+import concurrent.futures
 
 
 class DataLoader:
@@ -9,6 +10,19 @@ class DataLoader:
         self._files_dir = files_dir
         self._images_dir = images_dir
         self._pdf_elements = self._get_elements()
+
+    def _process_pdf(self, file_path):
+        pdf_elements = partition_pdf(
+            file_path,
+            chunking_strategy="by_title",
+            extract_images_in_pdf=True,
+            max_characters=3000,
+            new_after_n_chars=2800,
+            combine_text_under_n_chars=2000,
+            image_output_dir_path=self._images_dir
+        )
+
+        return pdf_elements
 
     def _get_elements(self):
         ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -25,19 +39,15 @@ class DataLoader:
             # Creamos una tarea con un total definido (por ejemplo, 100 unidades)
             task = progress.add_task("Procesando los archivos para el Data Loader...", 
                                      total=len(files))
+            
             elements = []
-            for f in files:
-                pdf_elements = partition_pdf(
-                    f,
-                    chunking_strategy="by_title",
-                    extract_images_in_pdf=True,
-                    max_characters=3000,
-                    new_after_n_chars=2800,
-                    combine_text_under_n_chars=2000,
-                    image_output_dir_path=self._images_dir
-                )
-                elements.extend(pdf_elements)
-                progress.update(task, advance=1)
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                futures = {executor.submit(self._process_pdf, pdf): pdf for pdf in files}
+
+                for future in concurrent.futures.as_completed(futures):
+                    el = future.result()
+                    elements.append(el)
+                    progress.advance(task)
         
         return elements
     
